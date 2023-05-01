6c531e406b94169a6fe834e1ce680d0688fca799
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-05-01 11:22:27 -0400

    Commit changes on json

--- config/config.js
+++ config/config.js
@@ -16,9 +16,9 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
+    "username": process.env.DEVELOPMENT_DATABASE_USER,
+    "password": process.env.DEVELOPMENT_DATABASE_PASSWORD, //probably null
+    "database": process.env.DEVELOPMENT_DATABASE_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   }
