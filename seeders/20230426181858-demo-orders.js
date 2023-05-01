1c45c272e8e0b8c97e71e8118a498c7e6ac73f36
Rodrigo Silva <rodrigob_silva@hotmail.com>
2023-05-01 15:11:36 -0400

    Adjust on demo-orders

--- seeders/20230426181858-demo-orders.js
+++ seeders/20230426181858-demo-orders.js
@@ -1,19 +1,16 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Orders', [
-      { userId: 1, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { userId: 2, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { userId: 3, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { userId: 4, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { userId: 5, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
-      { userId: 6, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 1, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 2, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 3, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 4, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 5, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
+      { user_id: 6, ordered_date: new Date(), createdAt: new Date(), updatedAt: new Date() },
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Orders', null, {});
-  }
+  },
 };
